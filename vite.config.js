51d2842474242cb6144bd72fdd34fb3067b9ce9e
Pravin  Rathod <143316584+PravinRathod009@users.noreply.github.com>
2026-05-15 08:56:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://quizzy-backend-seven.vercel.app/',
-        changeOrigin: true
-      }
+server: {
+  proxy: {
+    '/api': {
+      target: 'https://quizzy-backend-seven.vercel.app/',
+      changeOrigin: true
     }
   }
-});
+}
